13941bd786a50f2195970893a65f25e1586f952d
RAMYA S <ramma1833@gmail.com>
2025-08-11 15:20:50 +0530

    Add files via upload

---
+++ Array_Basic_Problems/Array_Challenge_1.java
@@ -0,0 +1,17 @@
+public class Array_Challenge_1
+{
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=s.nextInt();
+        }
+        for(int i=0;i<n;i++)
+        {
+           System.out.print(arr[i]+" ");
+        }
+    }
+}
